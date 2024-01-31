c53dd9becd8c06847cc995bc2f27290feae8cce6
d.homchenko <posharkow@mail.ru>
2024-01-31 08:54:40 +0300

    Changes have been made to lesson3

--- src/main/java/ru/homchenko/java/basic/homeworks/homework3/MainApplication.java
+++ src/main/java/ru/homchenko/java/basic/homeworks/homework3/MainApplication.java
@@ -2,8 +2,9 @@ package ru.homchenko.java.basic.homeworks.homework3;
 
 public class MainApplication {
     public static void main(String[] args) {
-        int[][]arr = {{1, 0, 3}, {2, 3, 5}};
-        int[][]arr1 = {{1, 0, 3, 5, 6}, {2, 3, 5, 6, 7}, {1, 2, 3, 4, 5}, {2, 2, 3, 3, 3}, {2, 2, 3, 3, 3}};
+        int[][] arr = {{1, 0, 3}, {2, 3, 5}};
+        int[][] arr1 = {{1, 0, 3, 5, 6}, {2, 3, 5, 6, 7}, {1, 2, 3, 4, 5}, {2, 2, 3, 3, 3}, {2, 2, 3, 3, 3}};
+        int[][] arr2 = {{1, 0, 3}};
         int result;
 
         result = sumOfPositiveElements(arr);
@@ -18,14 +19,17 @@ public class MainApplication {
 
         result = sumSecondStr(arr);
         System.out.println(result);
+
+        result = sumSecondStr(arr2);
+        System.out.println(result);
     }
 
     public static int sumOfPositiveElements(int[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if(arr[i][j] > 0) {
-                   sum += arr[i][j];
+                if (arr[i][j] > 0) {
+                    sum += arr[i][j];
                 }
             }
         }
@@ -34,17 +38,15 @@ public class MainApplication {
 
     public static void printSquare(int i) {
         for (int j = 0; j < i; j++) {
-            if (j == 0 || j == i-1){
+            if (j == 0 || j == i - 1) {
                 for (int k = 0; k < i; k++) {
                     System.out.print(" * ");
                 }
-            }
-            else {
+            } else {
                 for (int k = 0; k < i; k++) {
-                    if (k == 0 || k == i-1){
+                    if (k == 0 || k == i - 1) {
                         System.out.print(" * ");
-                    }
-                    else {
+                    } else {
                         System.out.print("   ");
                     }
                 }
@@ -80,12 +82,11 @@ public class MainApplication {
     public static int sumSecondStr(int[][] arr) {
         int idx = 1;
         int sum = 0;
-        if (arr[idx] != null) {
+        if (arr.length > idx && arr[idx] != null) {
             for (int i = 0; i < arr[idx].length; i++) {
                 sum += arr[idx][i];
             }
-        }
-        else{
+        } else {
             return -1;
         }
         return sum;
