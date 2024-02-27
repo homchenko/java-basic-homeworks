2fadefbe3681a34a04caf0d5207b90132f2bf502
d.homchenko <posharkow@mail.ru>
2024-02-27 08:48:22 +0300

    changed Exceptions-classes (Throwable -> RuntimeException)

--- src/main/java/ru/homchenko/java/basic/homeworks/homework8/AppArrayDataException.java
+++ src/main/java/ru/homchenko/java/basic/homeworks/homework8/AppArrayDataException.java
@@ -1,6 +1,9 @@
 package ru.homchenko.java.basic.homeworks.homework8;
 
-public class AppArrayDataException extends Throwable {
+//обычно рекомендуется наследоваться от Exception (для проверяемых исключений) или RuntimeException (для непроверяемых исключений),
+//потому что наследование от Throwable является избыточным, Throwable включает в себя и ошибки Error,
+// это уже не совсем подходит под логику.
+public class AppArrayDataException extends RuntimeException {
     public AppArrayDataException(String message) {
         super(message);
     }
--- src/main/java/ru/homchenko/java/basic/homeworks/homework8/AppArraySizeException.java
+++ src/main/java/ru/homchenko/java/basic/homeworks/homework8/AppArraySizeException.java
@@ -1,6 +1,9 @@
 package ru.homchenko.java.basic.homeworks.homework8;
 
-public class AppArraySizeException extends Throwable {
+//обычно рекомендуется наследоваться от Exception (для проверяемых исключений) или RuntimeException (для непроверяемых исключений),
+//потому что наследование от Throwable является избыточным, Throwable включает в себя и ошибки Error,
+// это уже не совсем подходит под логику.
+public class AppArraySizeException extends RuntimeException {
     public AppArraySizeException(String message) {
         super(message);
     }
