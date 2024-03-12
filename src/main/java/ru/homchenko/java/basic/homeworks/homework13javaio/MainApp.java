006716f2c8931b44acd78dc8b1cb14eb804884b8
d.homchenko <posharkow@mail.ru>
2024-03-12 12:47:45 +0300

    change lesson 13 (Пакет java.io, Класс File)

--- src/main/java/ru/homchenko/java/basic/homeworks/homework13javaio/MainApp.java
+++ src/main/java/ru/homchenko/java/basic/homeworks/homework13javaio/MainApp.java
@@ -6,9 +6,51 @@ import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
-        // Класс File - абстрактное представление пути к файлу или директории.
-        // Создание экземпляра класса не приводит к загрузке файла или директории в памāтþ.
         File directory = new File(System.getProperty("user.dir"));
+
+        if (checkDirectory(directory)) {
+            System.out.println("Введите название файла: ");
+            Scanner scanner = new Scanner(System.in);
+            String fileName = scanner.next();
+            scanner.nextLine();
+            readFile(fileName);
+
+            System.out.println("\nВведите текст для записи в файл: ");
+            String text = scanner.nextLine();
+            writeFile(fileName, text);
+        }
+    }
+
+    private static void writeFile(String fileName, String text) {
+        try (FileOutputStream fos = new FileOutputStream(fileName, true);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+        ) {
+            byte[] buffer = text.getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < buffer.length; i++) {
+                bos.write(buffer[i]);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void readFile(String fileName) {
+        try (FileInputStream fis = new FileInputStream(fileName);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             InputStreamReader isr = new InputStreamReader(bis);
+        ) {
+            int n = isr.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = isr.read();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static boolean checkDirectory(File directory) {
+        boolean ret = false;
 
         if (directory.isDirectory()) {
             File[] files = directory.listFiles();
@@ -16,47 +58,13 @@ public class MainApp {
             if (files != null) {
                 System.out.println("Вам доступны следующие файлы: ");
                 for (File file : files) {
-                    if (file.isFile() && file.getName().endsWith(".txt"))
+                    if (file.isFile() && file.getName().endsWith(".txt")) {
+                        ret = true;
                         System.out.println(file.getName());
+                    }
                 }
-            }
-
-            System.out.println("Введите название файла: ");
-            Scanner scanner = new Scanner(System.in);
-            String fileName = scanner.next(); // после считывания оставляет \n
-            scanner.nextLine(); // фиксит \n
-
-            try (FileInputStream fis = new FileInputStream(fileName);
-                 BufferedInputStream bis = new BufferedInputStream(fis);
-                 InputStreamReader isr = new InputStreamReader(bis); //для чтения с учетом кодировки
-            ) {
-                int n = isr.read();
-                while (n != -1) {
-                    System.out.print((char) n);
-                    n = isr.read();
-                }
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-
-            System.out.println("\nВведите текст для записи в файл: ");
-            String text = scanner.nextLine();
-
-            // Для того чтобы записать текст в конец существующего файла,
-            // необходимо использовать конструктор FileOutputStream(fileName, true)
-            // для открытия файла в режиме добавления (append mode).
-            // Таким образом, новый текст будет добавлен в конец файла, а не перезаписан.
-            try (FileOutputStream fos = new FileOutputStream(fileName, true);
-                 BufferedOutputStream bos = new BufferedOutputStream(fos);
-            ) {
-                byte[] buffer = text.getBytes(StandardCharsets.UTF_8);
-                for (int i = 0; i < buffer.length; i++) {
-                    bos.write(buffer[i]);
-                }
-            } catch (IOException exception) {
-                exception.printStackTrace();
             }
         }
+        return ret;
     }
 }
-
