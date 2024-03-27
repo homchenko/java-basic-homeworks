f48c1301b8ab64eb1dfcc73dd2b5805a23de70b3
d.homchenko <posharkow@mail.ru>
2024-03-27 10:01:06 +0300

    init lesson 15 (class Thread) changed 27.03

--- src/main/java/ru/homchenko/java/basic/homeworks/homework15multithreading/MainApp.java
+++ src/main/java/ru/homchenko/java/basic/homeworks/homework15multithreading/MainApp.java
@@ -1,7 +1,7 @@
 package ru.homchenko.java.basic.homeworks.homework15multithreading;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int l = 100_000_000;
         double array2[] = new double[l];
 
@@ -11,14 +11,33 @@ public class MainApp {
         System.out.println("Время выполнения 1: " + (time2 - time1));
 
         long time3 = System.currentTimeMillis();
-        Thread t1 = new Thread(() -> fillArr(0, 25_000_000, array2));
-        Thread t2 = new Thread(() -> fillArr(25_000_000, 50_000_000, array2));
-        Thread t3 = new Thread(() -> fillArr(50_000_000, 75_000_000, array2));
-        Thread t4 = new Thread(() -> fillArr(75_000_000, 100_000_000, array2));
+        Thread t1 = new Thread(() -> {
+            fillArr(0, 25_000_000, array2);
+            System.out.println("t1 finished");
+        });
+        Thread t2 = new Thread(() -> {
+            fillArr(25_000_000, 50_000_000, array2);
+            System.out.println("t2 finished");
+        });
+        Thread t3 = new Thread(() -> {
+            fillArr(50_000_000, 75_000_000, array2);
+            System.out.println("t3 finished");
+        });
+        Thread t4 = new Thread(() -> {
+            fillArr(75_000_000, 100_000_000, array2);
+            System.out.println("t4 finished");
+        });
+
         t1.start();
         t2.start();
         t3.start();
         t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
         long time4 = System.currentTimeMillis();
         System.out.println("Время выполнения 2: " + (time4 - time3));
     }
