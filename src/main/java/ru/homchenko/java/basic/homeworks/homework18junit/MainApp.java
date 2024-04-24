910eca88a74747e2b5892361966bb7ef7625c1cf
d.homchenko <posharkow@mail.ru>
2024-04-24 13:27:08 +0300

    init commit junit

---
+++ src/main/java/ru/homchenko/java/basic/homeworks/homework18junit/MainApp.java
@@ -0,0 +1,50 @@
+package ru.homchenko.java.basic.homeworks.homework18junit;
+
+public class MainApp {
+    public static void main(String[] args) {
+        int[] arr1 = {2, 2, 1, 1, 2, 2};
+        System.out.println(containsOneAndTwoOnly(arr1));
+        int[] resultArr1 = elemAfterLastOne(arr1);
+        for (int i : resultArr1) {
+            System.out.println(i);
+        }
+
+        int[] arr2 = {2, 2, 2, 2};
+        System.out.println(containsOneAndTwoOnly(arr2));
+        int[] resultArr2 = elemAfterLastOne(arr2);
+    }
+
+    public static int[] elemAfterLastOne(int [] arr) {
+        boolean foundOne = false;
+        int oneIdx = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] ==1) {
+                oneIdx = i;
+                foundOne = true;
+            }
+        }
+        if (!foundOne) {
+            throw  new RuntimeException("В массиве нет единиц.");
+        }
+        int[] resultArr = new int[arr.length - oneIdx - 1];
+        oneIdx++;
+        for (int i = 0; i < resultArr.length; i++) {
+            resultArr[i] = arr[oneIdx];
+            oneIdx++;
+        }
+        return resultArr;
+    }
+    public static boolean containsOneAndTwoOnly(int[] arr) {
+        boolean containsOne = false;
+        boolean containsTwo = false;
+        for (int i : arr) {
+            if(i !=1 && i != 2)
+                return false;
+            if(i == 1)
+                containsOne = true;
+            else if (i == 2)
+                containsTwo = true;
+        }
+        return containsOne && containsTwo;
+    }
+}
